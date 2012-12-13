76492606ea985a03a0f77644bf605d18a7408f66
eaglesakura <eagle.sakura@gmail.com>
2012-12-13 11:33:42 +0900

    階層構造を修正

---
+++ sources/core/include/jointcoding.h
@@ -0,0 +1,32 @@
+/*
+ * jointcoding.h
+ *
+ *  Created on: 2012/05/12
+ *      Author: Takeshi
+ */
+
+#ifndef JOINTCODING_H_
+#define JOINTCODING_H_
+
+// macro
+#include    "jcMacro.h"
+
+// C library
+/*
+#include "stdio.h"
+#include "math.h"
+#include "string.h"
+#include "stdarg.h"
+*/
+#include "jcLog.h"
+#include "jcSmartPtr.h"
+#include "jcImplCapsule.h"
+#include "jcString.h"
+
+// joint code library
+#include "jcObject.h"
+#include "jcException.h"
+#include "jcMutex.h"
+
+
+#endif /* JOINTCODING_H_ */
