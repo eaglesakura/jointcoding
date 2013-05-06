6d6ea17f9c7fce06ce309b45c0b735301724c45e
eaglesakura <eagle.sakura@gmail.com>
2013-05-06 17:01:47 +0900

    リネーム後のファイルを追加

---
+++ sources/core-gl/jc/framework/app/JointApplicationBase.cpp
@@ -0,0 +1,168 @@
+/*
+ * JointApplicationBase.cpp
+ *
+ *  Created on: 2013/05/06
+ */
+
+#include    "jc/framework/app/JointApplicationBase.h"
+
+namespace jc {
+namespace gl {
+
+JointApplicationBase::JointApplicationBase() {
+    flags.initialized = flags.destroyed = jcfalse;
+}
+
+/**
+ * サーフェイスが作成された
+ */
+void JointApplicationBase::dispatchSurfaceCreated(MDevice device) {
+    this->device = device;
+}
+
+/**
+ * サーフェイスのリサイズが行われた
+ */
+void JointApplicationBase::dispatchSurfaceResized(const s32 newWidth, const s32 newHeight) {
+    surfaceSize.x = newWidth;
+    surfaceSize.y = newHeight;
+}
+
+/**
+ * サーフェイスが廃棄された
+ */
+void JointApplicationBase::dispatchDestroy() {
+
+    flags.destroyed = jctrue;
+    try {
+        DeviceLock lock(device, jctrue);
+        if (!flags.initialized) {
+            // 初期化前であれば何もしない
+            return;
+        }
+
+        {
+            // アプリの解放を行う
+            onAppDestroy();
+        }
+
+        windowManager.reset();
+
+        // VRAMのプールを解放する
+        {
+            VRAM vram = getVRAM();
+            vram->gc();
+        }
+    } catch (Exception &e) {
+        jcloge(e);
+    }
+    device.reset();
+}
+
+/**
+ * 初期化処理を行う
+ */
+void JointApplicationBase::dispatchInitialize() {
+    // ウィンドウ生成
+    {
+        windowManager.reset(new WindowManager());
+        // 30fps〜60fpsで扱う
+        windowManager->setFramerateRange(30, 60);
+        windowManager->setWindowEventHandler(this);
+        windowManager->getWindowContext()->setSpriteManager(SpriteManager::createInstance(device));
+    }
+
+    onAppInitialize();
+}
+
+/**
+ * アプリケーションの休止処理を行う
+ * Activityの休止等
+ */
+void JointApplicationBase::dispatchPause() {
+    if (!flags.initialized) {
+        return;
+    }
+
+    try {
+        DeviceLock lock(device, jctrue);
+
+        onAppResume();
+    } catch (Exception &e) {
+        jcloge(e);
+    }
+}
+
+/**
+ * アプリケーションの再開処理を行う
+ * Activity再開等
+ */
+void JointApplicationBase::dispatchResume() {
+    if (!flags.initialized) {
+        return;
+    }
+
+    try {
+        DeviceLock lock(device, jctrue);
+
+        onAppResume();
+    } catch (Exception &e) {
+        jcloge(e);
+    }
+}
+
+/**
+ * メインループの外部呼び出しを行う
+ */
+void JointApplicationBase::dispatchMainLoop() {
+    if (!device || flags.destroyed) {
+        // デバイスが無ければ開始できない
+        return;
+    }
+
+    // デバイスの待ち合わせを行う
+    {
+        device->waitLockRequest(1, &flags.destroyed);
+        if (flags.destroyed) {
+            return;
+        }
+    }
+
+    // 実処理を行う
+    try {
+        DeviceLock lock(device, jctrue);
+
+        // 初期化が終わって無ければ初期化を行う
+        if (!flags.initialized) {
+            dispatchInitialize();
+            flags.initialized = jctrue;
+        }
+
+        // サーフェイスサイズチェックを行う
+        if (checkedSurfaceSize != surfaceSize) {
+            checkedSurfaceSize = surfaceSize;
+
+            // ビューポート更新
+            getState()->viewport(0, 0, surfaceSize.x, surfaceSize.y);
+
+            // アプリに処理を任せる
+            onAppSurfaceResized(surfaceSize.x, surfaceSize.y);
+        }
+
+        // フレーム定形処理を行う
+        {
+            {
+                onAppMainUpdate();
+            }
+            {
+                onAppMainRendering();
+            }
+        }
+    } catch (Exception &e) {
+        jcloge(e);
+    }
+}
+
+}
+}
+
---
+++ sources/core-gl/jc/framework/app/JointApplicationBase.h
@@ -0,0 +1,205 @@
+/*
+ * JointApplicationBase.h
+ *
+ *  Created on: 2013/05/06
+ */
+
+#ifndef JOINTAPPLICATIONBASE_H_
+#define JOINTAPPLICATIONBASE_H_
+
+#include    "jointcoding.h"
+#include    "jc/widget/window/WindowManager.h"
+#include    "jc/gl/GL2D.h"
+
+namespace jc {
+namespace gl {
+
+using namespace jc::view;
+
+/**
+ * マルチプラットフォーム共通アプリのベースクラス
+ */
+class JointApplicationBase: public Object, public WindowEventHandler {
+protected:
+    /**
+     * レンダリングデバイス
+     */
+    MDevice device;
+
+    /**
+     * ウィンドウクラス
+     */
+    MWindowManager windowManager;
+
+    struct {
+        /**
+         * 初期化済みであればtrue
+         */
+        jcboolean initialized;
+
+        /**
+         * 廃棄済みであればtrue
+         */
+        jcboolean destroyed;
+    } flags;
+
+    /**
+     * 最後にメインループを追加した時のサーフェイスサイズ
+     */
+    Vector2i checkedSurfaceSize;
+
+    /**
+     * メッセージで送られた実際のサーフェイスサイズ
+     */
+    Vector2i surfaceSize;
+
+public:
+    JointApplicationBase();
+
+    virtual ~JointApplicationBase() {
+    }
+
+public:
+    /* アクセサ */
+
+    /**
+     * レンダリングデバイスを取得する
+     */
+    virtual MDevice getDevice() const {
+        return device;
+    }
+
+    /**
+     * ウィンドウを取得する
+     */
+    virtual MWindowManager getWindowManager() const {
+        return windowManager;
+    }
+
+    /**
+     * コンテキストを取得する
+     */
+    virtual MWindowContext getWindowContext() const {
+        return windowManager->getWindowContext();
+    }
+
+    /**
+     * 現在のカレントステートを取得する
+     */
+    virtual MGLState getState() const {
+        return device->getState();
+    }
+
+    /**
+     * VRAMを取得する
+     */
+    virtual VRAM getVRAM() const {
+        return device->getVRAM();
+    }
+
+    /**
+     * ウィンドウサイズを取得する
+     */
+    virtual Vector2i getWindowSize() const {
+        return device->getSurfaceArea().wh();
+    }
+
+    /**
+     * スプライト管理クラスを取得する
+     */
+    virtual MSpriteManager getSpriteManager() const {
+        return windowManager->getWindowContext()->getSpriteManager();
+    }
+
+protected:
+    /* フレームワークライフサイクル */
+
+    /**
+     * アプリ初期化を行わせる
+     * メソッド呼び出し時点でデバイスロック済み。
+     */
+    virtual void onAppInitialize() = 0;
+
+    /**
+     * アプリのサーフェイスリサイズを行わせる
+     * メソッド呼び出し時点でデバイスロック済み。
+     */
+    virtual void onAppSurfaceResized(const s32 width, const s32 height) = 0;
+
+    /**
+     * メインループを行わせる
+     * メソッド呼び出し時点でデバイスロック済み
+     */
+    virtual void onAppMainUpdate() = 0;
+
+    /**
+     * メインレンダリングを行わせる。
+     * メソッド呼び出し時点でデバイスロック済み。
+     * フロントバッファ転送もメソッド内で行わせる。
+     */
+    virtual void onAppMainRendering() = 0;
+
+    /**
+     * アプリのメモリ解放を行う
+     * メソッド呼び出し時点でデバイスロック済み。
+     */
+    virtual void onAppDestroy() = 0;
+
+    /**
+     * アプリ休止処理
+     * 音声の休止等の処理を行う
+     */
+    virtual void onAppPause() = 0;
+
+    /**
+     * アプリの再開処理
+     * 音声の再開等の処理を行う
+     */
+    virtual void onAppResume() = 0;
+
+public:
+    /* アプリライフサイクル */
+
+    /**
+     * サーフェイスが作成された
+     */
+    virtual void dispatchSurfaceCreated(MDevice device);
+
+    /**
+     * アプリケーションの休止処理を行う
+     * Activityの休止等
+     */
+    virtual void dispatchPause();
+
+    /**
+     * アプリケーションの再開処理を行う
+     * Activity再開等
+     */
+    virtual void dispatchResume();
+
+    /**
+     * サーフェイスのリサイズが行われた
+     */
+    virtual void dispatchSurfaceResized(const s32 newWidth, const s32 newHeight);
+
+    /**
+     * サーフェイスが廃棄された
+     */
+    virtual void dispatchDestroy();
+
+    /**
+     * メインループの外部呼び出しを行う
+     */
+    virtual void dispatchMainLoop();
+protected:
+    /**
+     * 初期化処理を行う
+     */
+    virtual void dispatchInitialize();
+
+};
+
+}
+}
+
+#endif /* JOINTAPPLICATIONBASE_H_ */
