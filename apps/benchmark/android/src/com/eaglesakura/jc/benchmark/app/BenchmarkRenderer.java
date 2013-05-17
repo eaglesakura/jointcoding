8f4a3af37d589105c80a42c128dd8f7df96b3d17
eaglesakura <eagle.sakura@gmail.com>
2013-05-17 09:58:36 +0900

    自発的にメインループを開始するように修正

--- apps/benchmark/android/src/com/eaglesakura/jc/benchmark/app/BenchmarkRenderer.java
+++ apps/benchmark/android/src/com/eaglesakura/jc/benchmark/app/BenchmarkRenderer.java
@@ -1,6 +1,5 @@
 package com.eaglesakura.jc.benchmark.app;
 
-import com.eaglesakura.jc.egl.DeviceManager;
 import com.eaglesakura.jc.framework.app.JointApplicationRenderer;
 import com.eaglesakura.lib.jc.annotation.jnimake.JCClass;
 import com.eaglesakura.lib.jc.annotation.jnimake.JCMethod;
@@ -10,44 +9,6 @@ import com.eaglesakura.lib.jc.annotation.jnimake.JCMethod;
 public class BenchmarkRenderer extends JointApplicationRenderer {
 
     public BenchmarkRenderer() {
-    }
-
-    /**
-     * テクスチャを読み込む
-     */
-    @JCMethod(
-              nativeMethod = true)
-    native void loadTexture(DeviceManager deviceManager);
-
-    @Override
-    protected void startMainLoop() {
-        super.startMainLoop();
-
-        startSubLoop();
-    }
-
-    protected void startSubLoop() {
-        Thread thread = new Thread() {
-            @Override
-            public void run() {
-
-                //                DeviceManager deviceManager = getDeviceManager();
-                //                EGLWrapper egl = deviceManager.getEGLWrapper().createSlaveEGL();
-                //
-                //                AndroidUtil.log("surface");
-                //                EGLSurfaceWrapper surface = egl.createPBufferSurface(1, 1);
-                //
-                //                AndroidUtil.log("context");
-                //                EGLContextWrapper context = egl.createSharedContext(deviceManager.getEGLContextWrapper());
-
-                //                loadTexture(new DeviceManager(egl, context, surface));
-                //
-                //                context.dispose();
-                //                surface.dispose();
-            }
-        };
-        thread.setName("jc-sub0");
-        thread.start();
     }
 
     @JCMethod(
